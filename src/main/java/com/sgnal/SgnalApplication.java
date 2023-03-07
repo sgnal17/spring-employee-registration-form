3d1bf78c254c89402ce4408e317c5a6b668d3bff
sgnal17 <96966147+sgnal17@users.noreply.github.com>
2023-03-07 15:09:58 -0500

    model class added

---
+++ src/main/java/com/sgnal/SgnalApplication.java
@@ -0,0 +1,13 @@
+package com.sgnal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgnalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SgnalApplication.class, args);
+    }
+
+}
