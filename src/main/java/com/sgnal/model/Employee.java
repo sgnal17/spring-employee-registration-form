d1bded49d84e1830fba49c79aca34e3b00e03649
sgnal17 <96966147+sgnal17@users.noreply.github.com>
2023-03-08 12:32:45 -0500

    validation added

--- src/main/java/com/sgnal/model/Employee.java
+++ src/main/java/com/sgnal/model/Employee.java
@@ -3,15 +3,26 @@ package com.sgnal.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Employee {
+    //@NotNull--->@NotNull
+    //@NotEmpty--> @NotNull + @NotEmpty
+    //@NotBlank-->@NotNull + @NotEmpty + @NotBlank
+
+
+    @NotBlank
+    @Size(max=12,min=2)
     private String firstName;
     private String lastName;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthday;
     private String email;
     private String password;
